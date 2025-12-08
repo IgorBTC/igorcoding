eb7ca938f7d6eeec9483686b818ce0eaef597a00
IgorBTC <igorpavliukov03@gmail.com>
2025-12-09 00:24:13 +0400

    Сделано, но кодварс ругается

--- task_obr_words.py
+++ task_obr_words.py
@@ -1,13 +1,20 @@
+def spin_words(words, final_list):
+    for i in range (len(words)):
+        if len(words[i]) >= 5:
+            word = words[i]
+            final_word = word[::-1]
+            final_list.append(final_word)
+        else:
+            final_list.append(words[i])
+    return final_list
+
 string = str(input("Введите любую строку: "))
-
 word = "Ночь"
-final_string = "День"
+final_word = "День"
+final_list = []
 words = string.split()
+spin_words(words, final_list)
+final_string = ' '.join(final_list)
+print(final_string)
 
-for i in range (len(words)):
-    if len(words[i]) > 5:
-        word = words[i]
-        final_word = word[::-1]
-        final_string.append(final_word)
-    else:
-        final_string.append(words[i])
+
