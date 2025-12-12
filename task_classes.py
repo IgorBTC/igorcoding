f8d260a2052570ea05b862e62084dd2ca93b4411
IgorBTC <igorpavliukov03@gmail.com>
2025-12-13 01:19:22 +0400

    Почти всё

--- task_classes.py
+++ task_classes.py
@@ -1,6 +1,6 @@
 import random as rd
 
-class BankAccount:
+class BankAccount():
     def __init__(self, balance = 0, amount = 0, operation = ""):
         self.balance = balance
         self.user = {}
@@ -39,7 +39,7 @@ class BankAccount:
         self.amount = float(input("Введите сумму для пополнения: "))
         self.balance += self.amount
         if self.operation in self.transaction:
-            self.transaction[self.operation] = amount
+            self.transaction[self.operation] = self.amount
         return self.balance, self.transaction
     
     def withdraw(self):
@@ -51,6 +51,14 @@ class BankAccount:
     
     def get_transaction_history(self):
         print(self.transaction)
+    
+    def validate_amount(self):
+        if self.balance < 0:
+            print("Сумма отрицательная")
+        else: 
+            print("Сумма положительная")
+    
+    
         
 choice = int(input("Выберите действие: 1 - добавить владельца, 2 - удалить владельца, 3 - выход: "))
 
@@ -78,7 +86,7 @@ class SavingAccount(BankAccount):
         return self.interest_rate
 
 class CreditAccout(BankAccount):
-    def __init__(self, credit_limit = 300000, debt):
+    def __init__(self, credit_limit = 300000, debt = 0):
         super().__init__(self.balance, self.amount, self.operation)
         self.credit_limit = credit_limit
         self.debt = debt
@@ -98,3 +106,39 @@ class CreditAccout(BankAccount):
         return self.balance
     
     def available_balance(self):
+        print(f"Доступный баланс: {self.balance + self.credit_limit}")
+    
+class StudentAccount(BankAccount):
+    def __init__(self, university = "", adult, grant):
+        super().__init__(self.balance, self.amount, self.operation)
+        self.university = university
+        self.adult = adult
+        self.grant = grant
+    
+    def deposit(self):
+        super().deposit(self)
+        self.adult = int(input("Вы являетесь родителем? 1 - да, другое - нет: "))
+        self.amount = float(input("Введите сумму для пополнения: "))
+        if self.adult == 1:
+            self.balance += self.amount * 1.05
+        else:
+            self.balance += self.amount
+        if self.operation in self.transaction:
+            self.transaction[self.operation] = self.amount
+        return self.balance, self.transaction
+    
+    def withdraw(self):
+        super().withdraw(self)
+        while self.amount > 100000:
+            print("Ошибка! Сумма не должна превышать 100000")
+            self.amount = float(input("Введите сумму для снятия: "))
+        self.balance -= self.amount
+        if self.operation in self.transaction:
+            self.transaction[self.operation] = self.amount
+        return self.balance, self.transaction
+    
+
+    def apply_for_grant(self):
+        self.grant = int(input("Подаёте заявку на стипендию(1 - да, 2 - нет): "))
+        return self.grant
+    
